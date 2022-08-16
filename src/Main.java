5f9f7924000c1b5b4da0503b83d598480736db49
AnastasiyaShapka <shap-nas@yandex.ru>
2022-08-16 14:21:11 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 50000;
+        int mile = 20;
+
+        System.out.println("Начисленные мили: " + amount / mile);
+
+    }
+}
